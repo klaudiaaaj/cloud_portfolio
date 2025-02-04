07da565ca47fb5fc4ab1a5f5fec06599ef1571a1
klaudiaaaj <klaudiaaa.janecka@gmail.com>
2025-02-04 22:07:51 +0100

    assetsprefix

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV !== 'development') {
   nextConfig = {
     ...nextConfig,
     output: "export", 
-    assetPrefix: '/cloud_portfolio/',  
+    assetPrefix: 'https://klaudiaaaj.github.io/cloud_portfolio/',  
     basePath: '/cloudy-portoflio',
   };
 }
