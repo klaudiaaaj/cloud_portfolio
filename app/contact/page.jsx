6c77b20b457c84f2afb71aa33da3991bd0b36260
klaudiaaaj <klaudiaaa.janecka@gmail.com>
2025-02-05 12:32:20 +0100

    try on host

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -8,8 +8,10 @@ import {Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigg
 import React from 'react'
 import {FaPhoneAlt, FaEnvelope, FaMapMarkedAlt} from "react-icons/fa"
 import { motion } from 'framer-motion'
-import getConfig from 'next/config'
-const { publicRuntimeConfig } = getConfig()
+import getConfig from './../../next.config'
+
+const { publicRuntimeConfig } = getConfig
+
 
 const info =[
   {
@@ -42,7 +44,7 @@ return(
 <div className='flex flex-col xl:flex-row gap-[30px]'>
   <div className='xl:h-[60%] xl:w-[54%] order-2 xl:order-none'>
     <form className='flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl' action="https://api.web3forms.com/submit" method="POST">
-    <input type="hidden" name="access_key" value={publicRuntimeConfig.web3FormsKey}>
+        <inpu type="hidden" name="access_key" value={publicRuntimeConfig.web3FormsKey}/>
       <h3 className='text-4xl text-accent'>Let&apos;s connect!</h3>
       <p className="text-white/60">Please leave Yours contact details and message to me.  </p>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
@@ -59,23 +61,34 @@ return(
           <SelectGroup>
             <SelectLabel> Select a service
             </SelectLabel>
-              <SelectItem value ="est"> Web Development - Let me build/modify your website </SelectItem>
-              <SelectItem value ="est"> DevOps service - Let me help you with your infrastructure </SelectItem>
-              <SelectItem value ="est"> Database Engineering - Let me perform database, structural moderations </SelectItem>
-              <SelectItem value ="est"> Hire Me - Connect with me to give me a job proposal </SelectItem>
-              <SelectItem value ="est"> Software Consultation </SelectItem>
-              <SelectItem value ="est"> Other </SelectItem>
+              <SelectItem value ="op1"> Web Development - Let me build/modify your website </SelectItem>
+              <SelectItem value ="op2"> DevOps service - Let me help you with your infrastructure </SelectItem>
+              <SelectItem value ="op3"> Database Engineering - Let me perform database, structural moderations </SelectItem>
+              <SelectItem value ="op4"> Hire Me - Connect with me to give me a job proposal </SelectItem>
+              <SelectItem value ="op5"> Software Consultation </SelectItem>
+              <SelectItem value ="op6"> Other </SelectItem>
           </SelectGroup>
         </SelectContent>
       </Select>
       <Textarea className="h-[200px] w-full rounded-md border-white/10 bg-primary px-4 py-5 text-base placeholder:text-white/60 focus:border-accent/50 outline-none focus-visible:ring-offset-0 disabled-cursor-not-allowed disabled:opacity-50"
       placeholder="Type here your message">
       </Textarea>
-      <ButtonCon size="md" className="max-w-40">
+      <ButtonCon type="submit" size="md" className="max-w-40">
       Send Message
     </ButtonCon>
-    </input>
     </form>
+    <form action="https://api.web3forms.com/submit" method="POST">
+
+<input type="hidden" name="access_key" value="a2d5cd21-d9b7-4b1c-80c0-01553da086a8"/>
+
+<input type="text" name="name" required/>
+<input type="email" name="email" required/>
+<textarea name="message" required></textarea>
+<input type="hidden" name="redirect" value="https://web3forms.com/success"/>
+
+<button type="submit">Submit Form</button>
+
+</form>
 
   </div> 
   <div className='flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0'>
